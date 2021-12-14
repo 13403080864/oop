15fa414112492d2243dc1d9003e9b1d57babba48
wq <480524962@qq.com>
2021-12-14 14:25:39 +0800

    12.14

---
+++ src/main/java/com/Insert.java
@@ -0,0 +1,72 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author wq
+ * @since 2021-12-14 10:53
+ */
+
+public class Insert {
+    private static  String url = "jdbc:mysql://192.168.9.244:3306/wqtest?useUnicode=true&characterEncoding=UTF-8";
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String username = "root";
+    private static String password = "hellowin";
+    public static void main(String[] args) throws Exception {
+        //query();
+        insert();
+    }
+
+    public static void insert() throws Exception {
+        Connection conn = null;
+        Statement sta = null;
+        Class.forName(driver);
+        conn = DriverManager.getConnection(url, username, password);
+        sta = conn.createStatement();
+        //添加批处理
+        sta.addBatch("insert into user(username,birthday,sex,address) values('小明','1990-10-10','女','浙江省杭州是滨江区')");
+
+        //执行批处理
+         sta.executeBatch();
+    }
+
+        public  static void query()  {
+            Connection conn = null;
+            Statement sta = null;
+            try {
+                Class.forName(driver);
+                try {
+                    conn = DriverManager.getConnection(url, username, password);
+                    String sql = "select * from user";
+                    sta = conn.prepareStatement(sql);
+                    ResultSet resultSet = sta.executeQuery(sql);
+                    while (resultSet.next()) {
+                        System.out.println(resultSet.getString("id") + "\t"
+                                + resultSet.getString("username"));
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }finally {
+                if(sta !=null) {
+                    try {
+                        sta.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }if (conn != null){
+                    try {
+                        conn.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+}
--- src/main/java/com/InsertTest.java
+++ src/main/java/com/InsertTest.java
@@ -1,81 +1,81 @@
-//package com;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import java.sql.SQLException;
-//
-///**
-// * @author wq
-// * @since 2021-12-14 9:58
-// */
-//
-//public class InsertTest {
-//
-//    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        final String url = "jdbc:mysql://127.0.0.1/wqtest?useUnicode=true&characterEncoding=UTF-8";
-//        final String name = "com.mysql.jdbc.Driver";
-//        final String user = "root";
-//        final String password = "123456";
-//
-//        Connection conn = null;
-//        Class.forName(name);//指定连接类型
-//        conn = DriverManager.getConnection(url, user, password);//获取连接
-//        if (conn!=null) {
-//            System.out.println("获取连接成功");
-//            insert(conn);
-//        }else {
-//            System.out.println("获取连接失败");
-//        }
-//
-//    }
-//    public static void insert(Connection conn) {
-//
-//        // 开始时间
-//        Long begin = System.currentTimeMillis();
-//        // sql前缀
-//
-//        String prefix = "INSERT INTO users_copy2 (name,age,phoneNum,id_card) VALUES ";
-//        try {
-//            // 保存sql后缀
-//            StringBuffer suffix = new StringBuffer();
-//            // 设置事务为非自动提交
-//            conn.setAutoCommit(false);
-//            // 比起st，pst会更好些
-//            PreparedStatement pst = (PreparedStatement) conn.prepareStatement("");//准备执行语句
-//            // 外层循环，总提交事务次数
-//            for (int i = 1; i <= 10; i++) {
-//                suffix = new StringBuffer();
-//                // 第j次提交步长
-//                for (int j = 1; j <= 10; j++) {
-//                    // 构建SQL后缀
-//                    Object uutil = null;
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author wq
+ * @since 2021-12-14 9:58
+ */
+
+public class InsertTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        final String url = "jdbc:mysql://192.168.9.244:3306/wqtest?useUnicode=true&characterEncoding=UTF-8";
+        final String name = "com.mysql.jdbc.Driver";
+        final String user = "root";
+        final String password = "hellowin";
+
+        Connection conn = null;
+        Class.forName(name);//指定连接类型
+        conn = DriverManager.getConnection(url, user, password);//获取连接
+        if (conn!=null) {
+            System.out.println("获取连接成功");
+            insert(conn);
+        }else {
+            System.out.println("获取连接失败");
+        }
+
+    }
+    public static void insert(Connection conn) {
+
+        // 开始时间
+        Long begin = System.currentTimeMillis();
+        // sql前缀
+
+        String prefix = "INSERT INTO users_copy2 (name,age,phoneNum,id_card) VALUES ";
+        try {
+            // 保存sql后缀
+            StringBuffer suffix = new StringBuffer();
+            // 设置事务为非自动提交
+            conn.setAutoCommit(false);
+            // 比起st，pst会更好些
+            PreparedStatement pst = (PreparedStatement) conn.prepareStatement("");//准备执行语句
+            // 外层循环，总提交事务次数
+            for (int i = 1; i <= 10; i++) {
+                suffix = new StringBuffer();
+                // 第j次提交步长
+                for (int j = 1; j <= 10; j++) {
+                    // 构建SQL后缀
+                    Object uutil = null;
 //                    suffix.append("('"+RandomName.getChineseName()+"','20'"+ ",'女'"+",'138909078888'" +IdCardGenerator.areaCode +"),");
-//
-//                    //suffix.append("('"+UUID.randomUUID()+"','123456'"+ ",'男'"+",'教师'"+",'www.bbk.com'"+",'XX大学'"+",'"+"2016-08-12 14:43:26"+"','备注'" +"),");
-//                }
-//                // 构建完整SQL
-//                String sql = prefix + suffix.substring(0, suffix.length() - 1);
-//                // 添加执行SQL
-//                pst.addBatch(sql);
-//                // 执行操作
-//                pst.executeBatch();
-//                // 提交事务
-//                conn.commit();
-//                // 清空上一次添加的数据
-//                suffix = new StringBuffer();
-//            }
-//            // 头等连接
-//            pst.close();
-//            conn.close();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        // 结束时间
-//        Long end = System.currentTimeMillis();
-//        // 耗时
-//        System.out.println("数据插入花费时间 : " + (end - begin) / 1000 + " s");
-//        System.out.println("插入完成");
-//    }
-//}
-//
+
+                    //suffix.append("('"+UUID.randomUUID()+"','123456'"+ ",'男'"+",'教师'"+",'www.bbk.com'"+",'XX大学'"+",'"+"2016-08-12 14:43:26"+"','备注'" +"),");
+                }
+                // 构建完整SQL
+                String sql = prefix + suffix.substring(0, suffix.length() - 1);
+                // 添加执行SQL
+                pst.addBatch(sql);
+                // 执行操作
+                pst.executeBatch();
+                // 提交事务
+                conn.commit();
+                // 清空上一次添加的数据
+                suffix = new StringBuffer();
+            }
+            // 头等连接
+            pst.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // 结束时间
+        Long end = System.currentTimeMillis();
+        // 耗时
+        System.out.println("数据插入花费时间 : " + (end - begin) / 1000 + " s");
+        System.out.println("插入完成");
+    }
+}
+
